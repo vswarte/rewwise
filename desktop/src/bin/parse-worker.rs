56014206a993d6c8ee17d32e3b3d2ba3c29ef0bd
Vincent Swarte <vincentswarte@gmail.com>
2024-02-28 20:58:53 +0100

    Remove unused garbage

--- desktop/src/bin/parse-worker.rs
+++
@@ -1,6 +0,0 @@
-use gloo_worker::Registrable;
-use rewwise_worker::ParseWorker;
-
-fn main() {
-    ParseWorker::registrar().register();
-}
--- desktop/src/components/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod topbar;
-pub mod file_selector;
-
-pub use topbar::Topbar;
-pub use file_selector::OpenedFile;
-pub use file_selector::FileSelector;
